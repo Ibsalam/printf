bc6c785d617bf43b64e28ca6653286d911b1964a
daadesina <daadesina1@gmail.com>
2023-11-16 02:20:09 -0800

    done

--- main.c
+++ main.c
@@ -24,7 +24,6 @@ int main(void)
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
-    _printf("%b\n", 8);
     _printf("Unsigned octal:[%o]\n", ui);
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
